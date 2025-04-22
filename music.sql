fc481c214d163175dd2c84b58176054f44eff6bf
Estefang27 <123829468+Estefang27@users.noreply.github.com>
2025-04-21 23:40:02 -0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,20 @@
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    artist_id INTEGER,
+    FOREIGN KEY (artist_id) REFERENCES artists (id)
+);
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    album_id INTEGER,
+    track_number INTEGER,
+    duration_seconds INTEGER,
+    FOREIGN KEY (album_id) REFERENCES albums (id)
+);
